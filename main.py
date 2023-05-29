8fc61784945d73ec7a1d913e1a51a2b0313492c9
Checho-Locuras <jorge.ayala01@uptc.edu.co>
2023-05-29 08:54:06 -0500

    Segunda version de GUI en python

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import tkinter as tk
 import random
 import time
+from PIL import Image, ImageTk
+import threading
 
 class CarAssemblyLine:
 
     def __init__(self, root):
+        
         self.root = root
         self.root.title("Simulación de Línea de Ensamble de Carros")
         self.canvas_width = 700
         self.canvas_height = 618
+        self.canvas = tk.Canvas(root, width=self.canvas_width, height=self.canvas_height)
+        self.canvas.pack()
+
 
         # Estados de la linea de espera
         self.states = [
@@ -40,9 +46,6 @@ class CarAssemblyLine:
         self.w3p= 253
         self.h3p= 223
 
-        self.canvas = tk.Canvas(root, width=self.canvas_width, height=self.canvas_height)
-        self.canvas.pack()
-
         # DIbujamos los paneles
         self.canvas.create_rectangle(self.x_1panel, self.y_1panel, self.x_1panel + self.w1p, self.y_1panel + self.h1p, outline='black')
 
@@ -55,18 +58,7 @@ class CarAssemblyLine:
         # DIbujamos la linea de tiempo:
         self.canvas.create_line(152+47, 29+102, 152+47, 29+433+102)
 
-        self.statusStart = [1,2]
-
-        self.paint_status(self.statusStart)
-
-        self.car_width = 50
-        self.car_height = 30
-
-        self.car_states = []
-
-        self.statistics = {}
-
-    def paint_status(self, status):
+        # Dibujamos los estados
         posX = 0
         posY = 0
         i = 0
@@ -79,91 +71,69 @@ class CarAssemblyLine:
                 self.canvas.create_line(152+47, 102+68+(i*33), 152+94, 102+68+(i*33))
                 posX = 152+47+147
                 posY = 102+68+(i*33)
-
-            if i in status:
-                # Crear un Label
-                texto_label = state
-                label = tk.Label(root, text=texto_label, wraplength=100, relief="solid", borderwidth=1, highlightbackground="red")
-                self.canvas.create_window(posX-100, posY-20, anchor='nw', window=label)
-            else:
-                # Crear un Label
-                texto_label = state
-                label = tk.Label(root, text=texto_label, wraplength=100)
-                self.canvas.create_window(posX-100, posY-20, anchor='nw', window=label)
+            
+            # Crear un Label
+            texto_label = state
+            label = tk.Label(root, text=texto_label, wraplength=100)
+            self.canvas.create_window(posX-100, posY-20, anchor='nw', window=label)
             
             i += 1
+        # Se terminan de dibujar los estados
 
-    def create_car(self):
-        car_x = 0
-        car_y = self.canvas_height / 2 - self.car_height / 2
-        car = self.canvas.create_rectangle(car_x, car_y, car_x + self.car_width, car_y + self.car_height, fill="blue")
-        self.car_states.append(car)
+        self.car_width = 50
+        self.car_height = 30
 
-    def move_car(self):
-        for i, car in enumerate(self.car_states):
-            car_statistics = {}
+        self.car_states = []
 
-            for state in self.states:
-                self.canvas.itemconfig(car, fill="blue", outline="black")
-                self.canvas.create_text(self.car_width / 2, self.canvas_height / 2 + 20, text=state, anchor="center")
-                self.canvas.update()
-                time.sleep(random.uniform(0.5, 1.5))
-                self.canvas.itemconfig(car, fill="green", outline="black")
-                self.canvas.create_text(self.car_width / 2, self.canvas_height / 2 + 20, text=state, anchor="center")
-                self.canvas.update()
-                time.sleep(random.uniform(0.5, 1.5))
+        self.statistics = {}
 
-                entry_time = time.time()
-                in_service_time = random.uniform(1, 5)
-                time.sleep(in_service_time)
-                exit_time = time.time()
+    def paint_status(self, status, isOn):
+        posX = 0
+        posY = 0
+        if status % 2 == 0:
+            posX = 120+47
+            posY = 102+68+(status*33)
+        else:
+            posX = 152+47+147
+            posY = 102+68+(status*33)
+        # Crear un Label
+        texto_label = self.states[status]
+        if isOn:
+            label = tk.Label(root, text=texto_label, wraplength=100, relief="solid", borderwidth=1, highlightbackground="red")
+        else: 
+            label = tk.Label(root, text=texto_label, wraplength=100)
+        self.canvas.create_window(posX-100, posY-20, anchor='nw', window=label) 
 
-                car_statistics[state] = {
-                    "entry_time": entry_time,
-                    "in_service_time": in_service_time,
-                    "exit_time": exit_time
-                }
+    def init_status(self, status):
+        self.paint_status(status, True)
+        # El hilo se duerme por un tiempo
+        print("Esperando")
+        time.sleep(random.randint(1, 5)) 
+        print("Terminó de esperar")
+        self.paint_status(status, False)
+        self.statusBoolean[status] = True 
+        self.statusBoolean[status+1] = True 
+        self.init_status(status+1)
 
-            self.statistics[i] = car_statistics
-
-    def display_statistics(self):
-        statistics_window = tk.Toplevel(self.root)
-        statistics_window.title("Estadísticas")
-
-        for car_id, car_statistics in self.statistics.items():
-            car_label = tk.Label(statistics_window, text=f"Carro {car_id + 1}")
-            car_label.pack()
-
-            for state, times in car_statistics.items():
-                entry_time = times["entry_time"]
-                in_service_time = times["in_service_time"]
-                exit_time = times["exit_time"]
-
-                entry_label = tk.Label(statistics_window, text=f"{state}: Tiempo de entrada - {entry_time}")
-                entry_label.pack()
-
-                in_service_label = tk.Label(statistics_window, text=f"{state}: Tiempo en servicio - {in_service_time}")
-                in_service_label.pack()
-
-                exit_label = tk.Label(statistics_window, text=f"{state}: Tiempo de finalización - {exit_time}")
-                exit_label.pack()
-
-            separator = tk.Label(statistics_window, text="----------------------------------------")
-            separator.pack()
-
-    def start_simulation(self):
-        num_cars = 2
-        for _ in range(num_cars):
-            self.create_car()
-            self.move_car()
-
-        self.display_statistics()
+    def init_simulation(self):
+        self.statusBoolean = [True, True, True, True, True, True, True, True, True, True, True, True]
+        for i in range (100):
+            while self.statusBoolean[0] == False:
+                print("Esperando a que se desocupe el primer estado")
+                
+            self.statusBoolean[i] = False
+            # Crear un objeto de tipo Thread
+            mi_hilo = threading.Thread(target=self.init_status(0))
+            # Iniciar la ejecución del hilo
+            mi_hilo.start()
+            i+=1
+    
 
 if __name__ == "__main__":
     root = tk.Tk()
     app = CarAssemblyLine(root)
 
-    start_button = tk.Button(root, text="Iniciar simulación", command=app.start_simulation)
+    start_button = tk.Button(root, text="Iniciar simulación", command=app.init_simulation)
     start_button.pack()
 
     root.mainloop()
